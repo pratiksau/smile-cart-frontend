d85c4026b86036619e07657317848e8ba11d43c4
pratiksau <pratik.sau@bigbinary.com>
2025-02-04 14:54:48 +0530

    Added routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
+import Home from "components/Home";
+import PageNotFound from "components/PageNotFound";
+import { NavLink, Route, Switch } from "react-router-dom";
+
 import { Product } from "./components/Product";
 
 const App = () => (
-  <div>
-    <Product />
-  </div>
+  <>
+    <div className=" flex space-x-2 ">
+      <NavLink exact activeClassName=" underline font-bold" to="/">
+        Home
+      </NavLink>
+      <NavLink exact activeClassName=" underline font-bold" to="/product">
+        Product
+      </NavLink>
+    </div>
+    <Switch>
+      <Route exact component={Home} path="/" />
+      <Route exact component={Product} path="/product" />
+      <Route component={PageNotFound} path="*" />
+    </Switch>
+  </>
 );
 
 export default App;
---
+++ src/apis/products.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const show = () => axios.get("products/infinix-inbook-2");
+const productsApi = { show };
+export default productsApi;
