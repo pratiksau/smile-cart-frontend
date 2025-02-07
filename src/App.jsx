dcfadb3d30fc0fd97bd034ea3e67321cea6f6b05
pratiksau <pratik.sau@bigbinary.com>
2025-02-07 17:03:36 +0530

    Integrated Formik form into the checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Cart from "components/Cart";
+import Checkout from "components/Checkout";
 import PageNotFound from "components/commons/PageNotFound";
 import Product from "components/Product";
 import ProductList from "components/ProductList";
@@ -13,6 +14,7 @@ const App = () => (
     <Route exact component={Product} path={routes.products.show} />
     <Route exact component={ProductList} path={routes.products.index} />
     <Route exact component={Cart} path={routes.cart} />
+    <Route exact component={Checkout} path={routes.checkout} />
     <Redirect exact from={routes.root} to={routes.products.index} />
     <Route component={PageNotFound} path="*" />
   </Switch>
